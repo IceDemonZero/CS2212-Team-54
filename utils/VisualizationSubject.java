43e71ea07fa2bb0b6e4070c4665bd942cec3fb35
IceDemonZero <thanoseid@outlook.com>
2022-04-04 22:46:41 -0400

    Made corrections and added another design pattern(Observer).

--- utils/VisualizationSubject.java
+++ utils/VisualizationSubject.java
@@ -11,14 +11,25 @@ import java.util.List;
 public abstract class VisualizationSubject {
     private List<Observer> observers = 	new ArrayList<>();
 
+    /**
+     * Attach a new observer.
+     * @param observer
+     */
     public void attach(Observer observer) {
         observers.add(observer);
     }
 
+    /**
+     * Remove a new observer
+     * @param observer
+     */
     public void detach(Observer observer) {
         observers.remove(observer);
     }
 
+    /**
+     * Update the observers on a change.
+     */
     public void notifyObservers() {
         for (Observer observer : observers)
             observer.update(this);

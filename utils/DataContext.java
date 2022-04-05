2564c59a991b9d877252470fc8df4b21100c5842
IceDemonZero <thanoseid@outlook.com>
2022-04-04 22:31:19 -0400

    More comments and minor changes to cleanup the code. Put in the Observer Design pattern.

--- utils/DataContext.java
+++ utils/DataContext.java
@@ -1,11 +1,24 @@
 package utils;
 
+/**
+ * Context class for observer design pattern
+ * @author Jay.
+ */
 public class DataContext {
     private FetcherStrategy strategy;
 
+    /**
+     * Attach the strategy for this context.
+     * @param strategy
+     */
     public DataContext(FetcherStrategy strategy) {
         this.strategy = strategy;
     }
+
+    /**
+     * Change the strategy of this context.
+     * @param strategy
+     */
     public void setStrategy(FetcherStrategy strategy) {
         this.strategy = strategy;
     }
